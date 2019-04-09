dceecf9162a7e1e404ea91c695390269b59ab1f2
swcho9919 <swcho9919@gmail.com>
2019-04-09 16:46:31 +0900

    inital commit

---
+++ blog_books/blog_project/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+from django.utils import timezone
+# Create your models here.
+
+class Post(models.Model):
+    RATING = (
+        ('1', '1점'),
+        ('2', '2점'),
+        ('3', '3점'),
+        ('4', '4점'),
+        ('5', '5점'),
+    )
+    title = models.CharField(max_length=200)
+    review = models.TextField()
+    price = models.CharField(max_length=20)
+    score = models.CharField(max_length=1, choices=RATING)
+
+    def __str__(self):
+        return self.title   
