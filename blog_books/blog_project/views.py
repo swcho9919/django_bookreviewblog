80cd4713450d66f0679263c8b74479fc07628476
swcho9919 <swcho9919@gmail.com>
2019-04-29 10:32:21 +0900

    blog_proejct_ver.2

--- blog_books/blog_project/views.py
+++ blog_books/blog_project/views.py
@@ -21,3 +21,18 @@ def new(request):
 def detail(request, post_pk):
     post = Post.objects.get(pk = post_pk)
     return render(request, 'detail.html', {'post' : post})
+
+def edit(request, post_pk):
+    post = Post.objects.get(pk = post_pk)
+    if request.method == 'POST':
+        form = PostForm(request.POST, instance = post)
+        form.save()
+        return redirect('detail', post.pk)
+    else:
+        form = PostForm(instance = post)
+    return render(request, 'edit.html', {'form' : form, 'post' : post}, )
+
+def delete(request, post_pk):
+    post = Post.objects.get(pk = post_pk)
+    post.delete()
+    return redirect('home')
